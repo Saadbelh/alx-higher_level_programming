0d7b49061ed8ae8adcd184c154976bae20c99905
Saadbelh <saad.belhimer@gmail.com>
2023-10-31 17:32:39 -0700

    plkj

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    r = ""
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            print("{:c}".format(ord(i)-32), end="")
+        else:
+            print(i, end="")
+
