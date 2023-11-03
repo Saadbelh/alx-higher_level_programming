f36f3916fc527c3aeb7a201c7cf055931754966c
Saadbelh <saad.belhimer@gmail.com>
2023-11-02 17:06:30 -0700

    jbkhgkhghghssdsd

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    arguments = [int(arg) for arg in argv[1:]]
-    result = sum(arguments)
-    print(result)
+
+    import sys
+    total = 0
+    n = int(len(sys.argv))
+    for i in range(n - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
