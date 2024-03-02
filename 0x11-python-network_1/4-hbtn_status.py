47c3f494bc62e3c9d1ced955721e45a09502028e
Saadbelh <saad.belhimer@gmail.com>
2024-03-01 16:57:16 -0800

    cvxdone

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
-import requests
-
+"""0x11. Python - Network #1, task 4. What's my status? #1
+"""
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    from requests import get
+
+    response = get('https://intranet.hbtn.io/status')
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(
+        type(response.text), response.text))
